cfb43bc8fa520d03042e12f3942ef9a86d250e2f
Samuel Swarén <samuel.swaren@sogeti.com>
2022-11-01 14:17:20 +0100

    First init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>This is your generated React App</h1>
+            <h2>This is a template generated from Segelsfartygs GitHub-template</h2>
+            <h2>The template can be found on <a href="https://www.github.com/segelfartyg">GitHub</a>.</h2>
+        </div>
+    );
+}
+
+export default App;
